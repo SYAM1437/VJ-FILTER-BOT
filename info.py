b3051e8ad50812c5806fc753f5e757929ce2c937
SYAM1437 <syamgollapudi7@gmail.com>
2025-02-01 20:34:42 +0530

    info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.im.ge/2025/02/01/HS2aSa.photo-2025-02-01-19-42-32.jpeg')).split()
 
 
 # Admins & Users
