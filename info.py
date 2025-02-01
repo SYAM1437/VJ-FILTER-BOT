7a1f78947a8b5a52ce643b75e0499451befb5533
SYAM1437 <syamgollapudi7@gmail.com>
2025-02-01 19:32:06 +0530

    info.py

--- info.py
+++ info.py
@@ -84,10 +84,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/TDB251')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TDHMOVIESBOT')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Ibomma_admin_bot') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/RINGAN_OF_SASUKE')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
